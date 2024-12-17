9019663a1f48750cb0057209ea78718619c117a2
akcsantos <65388001+akcsantos@users.noreply.github.com>
2024-12-17 14:13:01 +0800

    initial setup of files

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import { useState } from "react";
+
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return <div>First commit</div>;
+}
+
+export default App;
